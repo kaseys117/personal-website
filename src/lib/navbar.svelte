d8d916dcb58c262c070ac2c255a6da097c757324
Kasey Soriano <104530889+kaseys117@users.noreply.github.com>
2025-07-05 13:54:02 -0500

    Finished Nav Bar

---
+++ src/lib/navbar.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+
+</script>
+
+<div class="uppernav">
+    <img src="/KS Logo.png" alt="KS Logo" width="75" height="75">
+    <nav class="flex items-center gap-6 tracking-wide">
+        <a href="/">Home</a>    
+        <a href="/aboutme">About Me</a>
+        <a href="/portfolio">Portfolio</a>
+        <a href="/blog">Blog</a>
+        <a href="/contact">Contact</a>
+    </nav>
+</div>
+
+<style>
+    .uppernav{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: #d0b7f7;
+    padding: 1rem 3rem 1rem;
+    border-radius: 1rem;    
+    }
+
+    nav {
+    font-size: 18px;
+    color: #454863;
+    }
+
+    a:hover {
+    color: #f1f1f1;
+    }
+
+</style>
+
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
 	import '../app.css';
+	import Navbar from '$lib/navbar.svelte'
+
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="mx-auto flex min-h-screen flex-col max-w-[68rem] px-2 pt-2">
+	<Navbar/>
+	<main class='flex-1 md:py-10 flex flex-col'>
+		{@render children()}
+	</main>
+</div>
