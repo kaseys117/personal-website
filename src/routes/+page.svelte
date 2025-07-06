e9283aabb732a2ddc53c4e332938b889d32ded97
Kasey Soriano <104530889+kaseys117@users.noreply.github.com>
2025-07-05 23:56:05 -0500

    Finished Home.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,95 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="home">
+	<div class="top">
+		<div class="box1">
+			<h2>Professional Summary</h2>
+			Highly motivated systems analyst and cybersecurity professional experienced in managing sensitive
+			data, ensuring compliance, and supporting security initiatives. Excels at clearly communicating
+			technical complexities, fostering collaboration across diverse teams, and proactively addressing
+			security challenges through research and continuous learning.
+		</div>
+
+		<div class="circle">
+			<img src="Me.png" width="200px" />
+		</div>
+
+		<div class="box2">
+			<h2>Why I Built This Site</h2>
+			I simply wanted to challenge myself. I had been working with some HTML in my current and previous
+			job and I found myself struggling, but I at least understood the gist of the code. One day, I had
+			a hit of inspiration and I told myself I would build my own personal website from scratch, meaning
+			no templates and no AI. Though I was presented with a lot of challenges and a learning curve, it
+			ended up working out. It's safe to say I'm satisfied with my progress and the result of this site
+			is evidence of my accomplishment.
+		</div>
+	</div>
+
+	<div class="bottom">
+		<div class="box3">
+			<h2>Current Role</h2>
+			I currently work at Sam Houston State University as a System Analyst. I deal with sensitive information
+			on the daily, troubleshoot technological issues, help automate processes, monitor software privileges
+			to ensure access is granted only to authorized users, and much more.
+		</div>
+
+		<div class="box4">
+			<h2>What's Next?</h2>
+			This website will always be a work in progress as I continue to pursue more projects and certifications.
+			The learning is never ending, especially in the field of cybersecurity. Now, it's time to check
+			out the rest of my website where you'll find out more about my personal and professional life.
+		</div>
+	</div>
+</div>
+
+<style>
+	.top,
+	.bottom {
+		display: flex;
+		width: auto;
+		justify-content: space-evenly;
+		margin-top: 1rem;
+		align-items: center;
+		padding: 0 1rem;
+	}
+
+	.box1 {
+		font-size: 15px;
+		color: #454863;
+		padding: 1.5rem 3rem 1.5rem 1.5rem;
+		border: 1px solid white;
+		border-radius: 1rem;
+		box-shadow: 0px 4px 10px #d3cedb;
+		background-color: #e5e2f4;
+		width: 40ch;
+		align-self: stretch;
+	}
+
+	.box2 {
+		font-size: 15px;
+		color: #454863;
+		padding: 1.5rem 1.5rem 1.5rem 1.5rem;
+		border: 1px solid white;
+		border-radius: 1rem;
+		box-shadow: 0px 4px 10px #d3cedb;
+		background-color: #e5e2f4;
+		width: 45ch;
+	}
+
+	.box3,
+	.box4 {
+		font-size: 14px;
+		color: #454863;
+		padding: 1.5rem 1.5rem 1.5rem 1.5rem;
+		border: 1px solid white;
+		border-radius: 1rem;
+		box-shadow: 0px 4px 10px #d3cedb;
+		background-color: #e5e2f4;
+		width: 50ch;
+	}
+
+	h2 {
+		font-size: 20px;
+		color: #454863;
+		font-weight: normal;
+		margin-bottom: .5rem;
+	}
+</style>
