09df3d188282c4faf8fe5c72ddfe6f6437e18151
Kasey Soriano <104530889+kaseys117@users.noreply.github.com>
2025-09-20 20:12:28 -0500

    Minor adjustments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,31 @@
+<h1>
+	<center> Home </center>
+</h1>
+
+<p class="text-center" style="color: #454863; margin-bottom: 2rem;">
+	 
+</p>
+
 <div class="home">
-	<div class="top">
-		<div class="box1">
-			<h2>Professional Summary</h2>
-			Highly motivated systems analyst and cybersecurity professional experienced in managing sensitive
-			data, ensuring compliance, and supporting security initiatives. Excels at clearly communicating
-			technical complexities, fostering collaboration across diverse teams, and proactively addressing
-			security challenges through research and continuous learning.
-		</div>
+	
+	
 
 		<div class="circle">
-			<img src="Me.png" width="200px" />
+			<img src="Me.png" width="250px" />
 		</div>
 
-		<div class="box2">
+<div class="intro">
+		<div class="box1">
 			<h2>Why I Built This Site</h2>
 			I simply wanted to challenge myself. I had been working with some HTML in my current and previous
-			job and I found myself struggling, but I at least understood the gist of the code. One day, I had
+			job and I found myself struggling, but I at least understood the gist. One day, I had
 			a hit of inspiration and I told myself I would build my own personal website from scratch, meaning
 			no templates and no AI. Though I was presented with a lot of challenges and a learning curve, it
 			ended up working out. It's safe to say I'm satisfied with my progress and the result of this site
 			is evidence of my accomplishment.
 		</div>
-	</div>
-
-	<div class="bottom">
-		<div class="box3">
-			<h2>Current Role</h2>
-			I currently work at Sam Houston State University as a System Analyst. I deal with sensitive information
-			on the daily, troubleshoot technological issues, help automate processes, monitor software privileges
-			to ensure access is granted only to authorized users, and much more.
-		</div>
-
-		<div class="box4">
+	
+		<div class="box2">
 			<h2>What's Next?</h2>
 			This website will always be a work in progress as I continue to pursue more projects and certifications.
 			The learning is never ending, especially in the field of cybersecurity. Now, it's time to check
@@ -41,26 +35,32 @@
 </div>
 
 <style>
-	.top,
-	.bottom {
+
+.home{
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+	.intro {
 		display: flex;
+		flex-direction: column;
+		gap: 1rem;
 		width: auto;
-		justify-content: space-evenly;
-		margin-top: 1rem;
 		align-items: center;
 		padding: 0 1rem;
 	}
 
+
 	.box1 {
 		font-size: 15px;
 		color: #454863;
-		padding: 1.5rem 3rem 1.5rem 1.5rem;
+		padding: 1.5rem 1.5rem 1.5rem 1.5rem;
 		border: 1px solid white;
 		border-radius: 1rem;
 		box-shadow: 0px 4px 10px #d3cedb;
 		background-color: #e5e2f4;
-		width: 40ch;
-		align-self: stretch;
+		width: 55ch;
 	}
 
 	.box2 {
@@ -71,19 +71,7 @@
 		border-radius: 1rem;
 		box-shadow: 0px 4px 10px #d3cedb;
 		background-color: #e5e2f4;
-		width: 45ch;
-	}
-
-	.box3,
-	.box4 {
-		font-size: 14px;
-		color: #454863;
-		padding: 1.5rem 1.5rem 1.5rem 1.5rem;
-		border: 1px solid white;
-		border-radius: 1rem;
-		box-shadow: 0px 4px 10px #d3cedb;
-		background-color: #e5e2f4;
-		width: 50ch;
+		width: 55ch;
 	}
 
 	h2 {
@@ -92,4 +80,5 @@
 		font-weight: normal;
 		margin-bottom: .5rem;
 	}
+	
 </style>
